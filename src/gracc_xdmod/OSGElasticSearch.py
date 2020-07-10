a90f297cec89237d646f2a4197f31a3de8ebdf67
Mats Rynge <mats@rynge.net>
2020-07-10 10:33:33 -0700

    The index is raw3 now

--- src/gracc_xdmod/OSGElasticSearch.py
+++ src/gracc_xdmod/OSGElasticSearch.py
@@ -49,9 +49,9 @@ class OSGElasticSearch(object):
         
         # only do this months index - small problem when crossing month
         # boundaries, but the data ignored is minor
-        # example: gracc.osg.raw-2016.11
+        # example: gracc.osg.raw3-2016.11
         now = datetime.utcnow()
-        index = 'gracc.osg.raw-' + now.strftime("%Y.%m")
+        index = 'gracc.osg.raw3-' + now.strftime("%Y.%m")
         
         log.info("Querying for data after " + start_time)
 
